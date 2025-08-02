d0a91ab594495fe7b30701db72d9751fb10b122c
Volodymyr Tesliuk <vovatesluk6a@gmail.com>
2025-08-02 14:52:06 +0300

    fix build issues

--- boards/arm/zsuzsa/white_screen_widget.c
+++ boards/arm/zsuzsa/white_screen_widget.c
@@ -3,69 +3,27 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/display/cfb.h>
-#include <zephyr/kernel.h>
-#include <zephyr/init.h>
-
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(white_screen, CONFIG_LOG_DEFAULT_LEVEL);
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if IS_ENABLED(CONFIG_ZMK_DISPLAY)
+#include <zmk/display.h>
 
-#define DISPLAY_NODE DT_CHOSEN(zephyr_display)
+#if IS_ENABLED(CONFIG_ZMK_DISPLAY_STATUS_SCREEN_CUSTOM)
 
-static const struct device *display_dev;
+static lv_obj_t *screen;
 
-static int white_screen_init(void) {
-    display_dev = DEVICE_DT_GET(DISPLAY_NODE);
-    
-    if (!device_is_ready(display_dev)) {
-        LOG_ERR("Display device not ready");
-        return -ENODEV;
-    }
-
-    LOG_INF("Display device ready");
-
-    // Get display capabilities
-    struct display_capabilities caps;
-    display_get_capabilities(display_dev, &caps);
-    
-    LOG_INF("Display: %dx%d, %d bpp", caps.x_resolution, caps.y_resolution, caps.supported_pixel_formats);
-
-    // Clear display with white
-    display_blanking_off(display_dev);
-    
-    // Fill entire screen with white pixels
-    uint8_t *white_buffer = k_malloc(caps.x_resolution * caps.y_resolution / 8); // 1 bit per pixel
-    if (white_buffer) {
-        // Set all bits to 1 for white (for monochrome displays)
-        memset(white_buffer, 0xFF, caps.x_resolution * caps.y_resolution / 8);
+lv_obj_t *zmk_display_status_screen(void) {
+    if (screen == NULL) {
+        screen = lv_obj_create(NULL);
         
-        struct display_buffer_descriptor desc = {
-            .buf_size = caps.x_resolution * caps.y_resolution / 8,
-            .width = caps.x_resolution,
-            .height = caps.y_resolution,
-            .pitch = caps.x_resolution,
-        };
+        // Set the screen background to white
+        lv_obj_set_style_bg_color(screen, lv_color_white(), LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
         
-        int ret = display_write(display_dev, 0, 0, &desc, white_buffer);
-        if (ret < 0) {
-            LOG_ERR("Failed to write to display: %d", ret);
-        } else {
-            LOG_INF("White screen displayed successfully");
-        }
-        
-        k_free(white_buffer);
-    } else {
-        LOG_ERR("Failed to allocate display buffer");
-        return -ENOMEM;
+        LOG_INF("Custom white screen created");
     }
     
-    return 0;
+    return screen;
 }
 
-SYS_INIT(white_screen_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
-
-#endif /* IS_ENABLED(CONFIG_ZMK_DISPLAY) */
+#endif /* IS_ENABLED(CONFIG_ZMK_DISPLAY_STATUS_SCREEN_CUSTOM) */
